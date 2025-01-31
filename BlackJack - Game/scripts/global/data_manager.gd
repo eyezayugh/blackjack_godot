702bfbf3262e5c7e705662f6b984986e7dbbda75
Isaiah Fernando <72065354+eyezayugh@users.noreply.github.com>
2025-01-31 16:21:10 +0800

    main menu functionalities completed, setting sound function still needs doing
    main menu functionalties done, but in the settings menu still need to actually turn off and on sound when button is pressed.

---
+++ BlackJack - Game/scripts/global/data_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var sound_settings = true;
+
+func reset_all_data():
+	pass
--- BlackJack - Game/scripts/menu/main_menu.gd
+++ BlackJack - Game/scripts/menu/main_menu.gd
@@ -1,26 +1,20 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var fullscreen = false;
+var sound = true;
 
 
 func _on_new_game_button_pressed():
-	pass # Replace with function body.
+	DataManager.reset_all_data()
+	get_tree().change_scene_to_file("res://Scene/game/game.tscn")
 
 
 func _on_continue_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scene/game/game.tscn")
 
 
 func _on_settings_button_pressed():
-	pass # Replace with function body.
+	$settings_popup.show()
 
 
 func _on_about_button_pressed():
@@ -33,3 +27,29 @@ func _on_exit_button_pressed():
 
 func _on_about_back_button_pressed():
 	$about_popup.hide()
+
+
+func _on_settings_back_button_pressed():
+	$settings_popup.hide()
+
+
+# TODO : enable and disable the game sound
+func _on_settings_sound_button_pressed():
+	sound = !sound
+	
+	if sound:
+		$settings_popup/settings_items/sounnd_settings/settings_sound_button.text = "OFF"
+	else:
+		$settings_popup/settings_items/sounnd_settings/settings_sound_button.text = "ON"
+
+
+func _on_settings_fullscreen_button_pressed():
+	fullscreen = !fullscreen
+	
+	if(fullscreen):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		$settings_popup/settings_items/fullscreen_settings/settings_fullscreen_button.text = "ENABLE"
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		$settings_popup/settings_items/fullscreen_settings/settings_fullscreen_button.text = "DISABLE"
+	
