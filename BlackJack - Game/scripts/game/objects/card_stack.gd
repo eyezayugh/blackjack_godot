f784dfba7c78ef4ce274161ca3e2133ae5b54623
Isaiah Fernando <72065354+eyezayugh@users.noreply.github.com>
2025-02-01 15:33:18 +0800

    finished card stack scene

--- BlackJack - Game/scripts/game/objects/card_stack.gd
+++ BlackJack - Game/scripts/game/objects/card_stack.gd
@@ -5,27 +5,36 @@ var card_scene = preload("res://Scene/game/objects/card.tscn")
 var current_x_offset = 0.0
 var x_step = 2.7
 
-var amount_off_card = 0.0
+var amount_of_cards = 0.0
 
 var value = 0
-var ace_value = false
+var ace_value = 0
+var has_ace_value = false
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		add_card(0)
+var is_dealer = true
 
 func add_card(id):
 	var new_card = card_scene.instantiate()
 
 	new_card.transform.origin.x = current_x_offset
+	
+	if is_dealer && amount_of_cards == 0:
+		new_card.rotation.z = deg_to_rad(180)
+	
 	current_x_offset += x_step
 
 	$cards.add_child(new_card)
 
 	new_card.set_card(id)
 	$cards.transform.origin.x = -(current_x_offset-x_step)/2
+	
+	amount_of_cards += 1
 
 	calculate_value();
+	
+func show_dealer_cards():
+	if is_dealer:
+		$cards.get_child(0).rotation.z = 0
 
 func calculate_value():
 	var total_value = 0;
@@ -34,9 +43,21 @@ func calculate_value():
 		while card_value > 13:
 			card_value -= 13
 		
-		if card_value >  10:
+		if card_value >= 10:
 			total_value += 10
 		else:
-			total_value += card_value + 1
+			total_value += card_value+1
 			
-	print(total_value)
+		if card_value == 0:
+			has_ace_value = true
+		
+	if ace_value && total_value+10 > 21:
+		has_ace_value = false
+		
+	value = total_value
+	ace_value = total_value + 10
+			
+	if !has_ace_value:
+		print(total_value)
+	else:		
+		print(str(total_value) + "/" + str(ace_value + 10))
